0cbfeba83972278a75ac18882214f71e9cef2f0c
jimmykehs <jamesekehs@gmail.com>
2021-06-24 14:55:36 -0500

    Added API routes for register and login, added 2 DB functions (verifyUniqueUser & getUserByUsername)

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-const apiRouter = require('express').Router();
+const apiRouter = require("express").Router();
+const usersRouter = require("./Users");
+const productsRouter = require("./Products");
+require("dotenv").config();
 
 apiRouter.get("/", (req, res, next) => {
   res.send({
-    message: "API is under construction!"
+    message: "API is under construction!",
   });
+});
+
+apiRouter.use("/users", usersRouter);
+apiRouter.use("/products", productsRouter);
+
+apiRouter.use((error, req, res, next) => {
+  res.send(error);
 });
 
 module.exports = apiRouter;
